5d94946385a52c3255a4a3e1cb9b09a003142c12
unknown <dinushamadhuranga77@gmail.com>
2025-04-23 03:05:30 +0530

    Event Binding

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from "./navbar/navbar.components";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppNavbar],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularApp';
+  title: string = "This load dianamically";
+  imgUrl: string = 'https://www.humaneworld.org/sites/default/files/styles/sa_social_media_facebook/public/2022-09/wild-rabbit-166068.jpg?h=20b84feb&itok=AySMKYcT';
+  isDisabled: boolean = true;
+  isActive : boolean = true;
+  fruitName : string = "Apple";
+
+  buttonClick() {
+    console.log("button Clickeed");
+  }
+  enter(){
+    console.log("enter pressed");
+  }
+
 }
