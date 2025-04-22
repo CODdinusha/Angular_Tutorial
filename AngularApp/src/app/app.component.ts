111a19f9cd8489a7743fc24d148a21543f42dcf9
unknown <dinushamadhuranga77@gmail.com>
2025-04-22 20:47:52 +0530

    1

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from "./navbar/navbar.components";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ AngularApp/src/app/navbar/navbar.components.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navbar',
+    standalone:true,
+    template:'<h1>Navbar Components<h1>'
+
+})
+
+export class AppNavbar {
+   
+}
