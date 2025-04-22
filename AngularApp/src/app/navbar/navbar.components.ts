3c64a04735ec6daa6556eb8aba8fdef062fef673
unknown <dinushamadhuranga77@gmail.com>
2025-04-22 21:26:03 +0530

    2

--- AngularApp/src/app/navbar/navbar.components.ts
+++ AngularApp/src/app/navbar/navbar.components.ts
@@ -3,8 +3,16 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-navbar',
     standalone:true,
-    template:'<h1>Navbar Components<h1>'
-
+    // template:`<h1>Navbar Component</h1>
+    //           <p>paragraph</p>
+    //           <button>Submit</button>
+    //           `,
+    // styles: `h1{
+    //             color: red;
+    //             background:black;
+    // }`,
+    templateUrl:'./navbar.component.html',
+    styleUrl: './navbar.component.css'
 })
 
 export class AppNavbar {
